a7defc2bf156e8612d56afc6cb85a177e52a4de8
Caedes <zhunter631@gmai.com>
2025-03-19 05:20:18 +0800

    new stuff  in category search its now working and also for searching is also working and thumbnail css have add for thumbnail container

--- CATEGORY SEARCHING.js
+++ CATEGORY SEARCHING.js
@@ -1,42 +1,17 @@
 let API_URL = "";
 let data = "";
+
 document.querySelectorAll(".category-btn").forEach((button) => {
   button.addEventListener("click", async function () {
     let workoutname = this.getAttribute("data-value"); // Get value from button
 
     if (!workoutname) return;
-    API_URL = `https://exercisedb.p.rapidapi.com/exercises/name/${encodeURIComponent(
-      workoutname
-    )}`;
-
-    try {
-      const response = await fetch(API_URL, {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "87f5e59955mshdae3a211e701e93p1d5c76jsn6813f7326822",
-          "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-        },
-      });
-
-      const data = await response.json();
-      console.log("Fetch Data:", data);
-
-      if (!data || data.length === 0) {
-        alert("no results");
-        return;
-      }
-
-      fetchExerciseGifs(data);
-    } catch (error) {
-      console.error("Error fetching", error);
-    }
+    window.location.href = `?search=${encodeURIComponent(workoutname)}`;
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
-
   const workoutname = urlParams.get("search");
 
   if (!workoutname) {
@@ -45,6 +20,31 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   console.log("Searching for...:", workoutname);
   console.log("API URL", API_URL);
+  API_URL = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${encodeURIComponent(
+    workoutname
+  )}`;
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "87f5e59955mshdae3a211e701e93p1d5c76jsn6813f7326822",
+        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Fetch Data:", data);
+
+    if (!data || data.length === 0) {
+      alert("no results");
+      return;
+    }
+
+    fetchExerciseGifs(data);
+  } catch (error) {
+    console.error("Error fetching", error);
+  }
 });
 // get the string value from the user
 
